0328310cbdf17155b95de430d8adbd298ffbe970
Thomas Can <can.thomas@orange.fr>
2021-08-17 08:27:27 +0200

    Navigation des Tags dynamique dans le Header

--- docs/js/index.js
+++ docs/js/index.js
@@ -15,10 +15,49 @@ fetch('FishEyeData.json').then( (data) => {
     photographersList = result.photographers.filter(photographerData => photographerData.tags.includes(tag));
   }
 
+  let tagsList = [];
+
   for (let photographerData of photographersList) {
     const photographer = new Photographer(photographerData.id, photographerData.portrait, photographerData.name, photographerData.city, photographerData.country, photographerData.tagline, photographerData.price, photographerData.tags);
     photographer.renderHomepage();
+
+    photographerData.tags.forEach(element => {
+      tagsList.push(element);
+    });
   }
+
+  const tagsListUnique = new Set(tagsList);
+
+  console.log(tagsListUnique);
+
+  // Tag Nav
+  tagsListUnique.forEach(element => {
+    // Création d'éléments
+    const ulTag = document.querySelector('.tags');
+    const liTag = document.createElement('li');
+    const aTag = document.createElement('a');
+
+    liTag.classList.add('tags__item');
+    aTag.classList.add('tags__link');
+
+    aTag.setAttribute('href', `?tag=${element}`);
+    aTag.innerHTML = `#${element}`;
+
+    ulTag.append(liTag);
+    liTag.append(aTag);
+    
+    // Tag en surbrillance
+    const url = new URL(window.location);
+    const searchParams = new URLSearchParams(url.search);
+    const tag = searchParams.get('tag');
+    
+    if (aTag.textContent.includes(tag)) {
+      liTag.style.backgroundColor = '#D3573C';
+      liTag.style.color = '#000';
+      liTag.setAttribute('aria-current', 'true');
+    }
+  });
+
 }).catch( (err) => {
   alert(err);
 });
